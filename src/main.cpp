1d6930a7ebd76990172d086d2e3c9bcc15afab40
Enzobu <enzo.palermo@epsi.fr>
2024-09-26 23:17:11 +0200

    setup wifi

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,30 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
+WiFiClient client;
 void setup() {
+    Serial.begin(115200);        // Initialisation de la communication série
+    SPI.begin();                 // Initialisation de la communication SPI
+    rfid.PCD_Init();             // Initialisation du module RFID
+    Serial.println("Placez votre carte ou porte-clé RFID.");
+    // Connexion au Wi-Fi
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    Serial.print("Connexion au Wifi ...");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        digitalWrite(LED_BUILTIN, LOW);   // Allume la LED
+        delay(50);                // Pendant 50 ms
+        digitalWrite(LED_BUILTIN, HIGH);    // Éteint la LED
+        delay(450);               // Pause de 450 ms
+        Serial.print(".");
+    }
+
+    Serial.println("\n");
+    Serial.println("Connexion Wifi réussie");
+    Serial.print("Adresse IP : ");
+    Serial.println(WiFi.localIP());
 }
 void loop() {
 }
