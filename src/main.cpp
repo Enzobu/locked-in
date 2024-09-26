94ddbe7544b94464f5c493c6626f9e781ca621bf
Enzobu <enzo.palermo@epsi.fr>
2024-09-26 23:15:19 +0200

    add lib

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+void setup() {
+}
+void loop() {
+}
